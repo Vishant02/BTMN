5fc0ea89432f80442a8fe8831aacb790ce17e625
vishantm <vishantm@antiersolutions.com>
2022-09-07 13:12:26 +0530

    two

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,28 +1,32 @@
+const asyncHandler = require('express-async-handler');
+
  //@desc Get Goals
  //@route Get /api/goals
  //@access Private
- const getGoals = (req,res)=>{
+ const getGoals = asyncHandler(async (req,res)=>{
     res.status(200).json({message: 'Get Goals'});
- };
-
+ });
   //@desc Set Goals
  //@route POST /api/goals
  //@access Private
- const setGoals = (req,res)=>{
+ const setGoals = asyncHandler(async (req,res)=>{
+    if(!req.body.text){
+        res.status(400)
+        throw new Error('Pls add a text field');
+    }
     res.status(200).json({message: 'Set Goals'});
- };
-
+ });
   //@desc Update Goals
  //@route PUT /api/goals/:id
  //@access Private
- const updateGoals = (req,res)=>{
+ const updateGoals = asyncHandler(async (req,res)=>{
     res.status(200).json({message: `Update Goals ${req.params.id}`});
- };
+ });
 
   //@desc Delete Goals
  //@route DELETE /api/goals/:ID
  //@access Private
- const deleteGoals = (req,res)=>{
+ const deleteGoals = asyncHandler(async (req,res)=>{
     res.status(200).json({message: `Delete Goals ${req.params.id}`});
- };
+ });
  module.exports = {getGoals,setGoals,updateGoals,deleteGoals};
